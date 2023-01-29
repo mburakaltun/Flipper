982fdb014edf8575f5d4462597f3341a5fd5404d
MUHAMMED BURAK ALTUN <MALTUN15@ku.edu.tr>
2023-01-29 21:35:23 +0300

    Initial commit

---
+++ src/main/java/com/caomba/Flipper/FlipperApplication.java
@@ -0,0 +1,13 @@
+package com.caomba.Flipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caomba/Flipper/FlipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caomba.Flipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
